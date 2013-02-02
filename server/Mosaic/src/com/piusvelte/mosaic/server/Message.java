cff3ed68db92919ef6239c74fd97b3125b3dd590
piusvelte <piusvelte@gmail.com>
2013-02-01 22:34:47 -0500

    add user class

--- server/Mosaic/src/com/piusvelte/mosaic/server/Message.java
+++ server/Mosaic/src/com/piusvelte/mosaic/server/Message.java
@@ -19,9 +19,22 @@
  */
 package com.piusvelte.mosaic.server;
 
+import java.util.List;
+
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.labs.repackaged.org.json.JSONArray;
+import com.google.appengine.labs.repackaged.org.json.JSONObject;
+
 public class Message {
 
 	public static final String kind = "message";
+	public static final String id = "id";
 	public static final String body = "body";
 	public static final String created = "created";
 	public static final String latitude = "latitude";
@@ -34,5 +47,76 @@ public class Message {
 	public static final String expiry = "expiry";
 	public static final String visits = "visits";
 	public static final String reports = "reports";
+	public static final String flagged = "flagged";
+	
+	public static Query getMessagesQuery(String latitude, String longitude, String self, Key userKey) throws Exception {
+		Query query = new Query(Message.kind);
+		try {
+			if (Boolean.parseBoolean(self)) {
+				query.setFilter(getCoordinatesFilter(latitude, longitude));
+				query.setAncestor(userKey);
+			} else
+				query.setFilter(getNonFlaggedFilter(getCoordinatesFilter(latitude, longitude)));
+		} catch (Exception e) {
+			if ((userKey != null) && Boolean.parseBoolean(self))
+				query.setAncestor(userKey);
+			else
+				throw new Exception("invalid parameters");
+		}
+		return query;
+	}
+	
+	private static Filter getNonFlaggedFilter(Filter coordinatesFilter) {
+		return CompositeFilterOperator.and(coordinatesFilter, new FilterPredicate(Message.flagged, FilterOperator.NOT_EQUAL, true));
+	}
+	
+	private static Filter getCoordinatesFilter(String latitude, String longitude) throws Exception {
+		if ((latitude != null) && (longitude != null))
+			return CompositeFilterOperator.and(getCoordinateFilter(latitude, Message.minlatitude, Message.maxlatitude),
+					getCoordinateFilter(longitude, Message.minlongitude, Message.maxlongitude));
+		else
+			throw new Exception("no coordinates");
+	}
+	
+	private static Filter getCoordinateFilter(String coordinate, String min, String max) {
+		return CompositeFilterOperator.and(new FilterPredicate(coordinate, FilterOperator.GREATER_THAN_OR_EQUAL, min),
+				new FilterPredicate(coordinate, FilterOperator.LESS_THAN_OR_EQUAL, max));
+	}
+	
+	public static String jsonFromEntities(List<Entity> messages) throws Exception {
+		JSONArray jarr = new JSONArray();
+		for (Entity message : messages) {
+			try {
+				jarr.put(jsonFromEntity(message));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return jarr.toString();
+	}
+	
+	public static String jsonFromEntity(Entity message) throws Exception {
+		JSONObject jobj = new JSONObject();
+		jobj.put(Message.id, message.getKey().getId());
+		jobj.put(Message.body, message.getProperty(Message.body));
+		jobj.put(Message.created, message.getProperty(Message.created));
+		jobj.put(Message.latitude, message.getProperty(Message.latitude));
+		jobj.put(Message.longitude, message.getProperty(Message.longitude));
+		jobj.put(Message.radius, message.getProperty(Message.radius));
+		jobj.put(Message.expiry, message.getProperty(Message.expiry));
+		jobj.put(MosaicUser.kind, MosaicUser.jsonFromKey(message.getParent()));
+		return jobj.toString();
+	}
+	
+	public static Entity entityFromRequest(String body, String lat, String lon, String rad, String exp) {
+		Entity newMessage = new Entity(kind);
+		newMessage.setProperty(Message.body, body);
+		newMessage.setProperty(Message.latitude, lat);
+		newMessage.setProperty(Message.longitude, lon);
+		newMessage.setProperty(Message.radius, rad);
+		newMessage.setProperty(Message.expiry, exp);
+		//TODO: calc range
+		return newMessage;
+	}
 	
 }
--- server/Mosaic/src/com/piusvelte/mosaic/server/MessagesServlet.java
+++ server/Mosaic/src/com/piusvelte/mosaic/server/MessagesServlet.java
@@ -20,7 +20,6 @@
 package com.piusvelte.mosaic.server;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -28,47 +27,31 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
-import com.google.appengine.api.datastore.Query.Filter;
-import com.google.appengine.api.datastore.Query.FilterOperator;
-import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.appengine.api.oauth.OAuthRequestException;
 import com.google.appengine.api.oauth.OAuthServiceFactory;
 import com.google.appengine.api.users.User;
-import com.google.appengine.labs.repackaged.org.json.JSONArray;
 import com.google.appengine.labs.repackaged.org.json.JSONException;
 import com.google.appengine.labs.repackaged.org.json.JSONObject;
 
 public class MessagesServlet extends HttpServlet {
 	
-	public static final String kind = "messages";
-	public static final String key = "key";
-	public static final String body = "body";
-	public static final String created = "created";
-	public static final String latitude = "latitude";
-	public static final String longitude = "longitude";
-	public static final String radius = "radius";
-	public static final String expiry = "expiry";
-	public static final String user = "user";
-	public static final String userid = "userid";
-	
 	private static final long serialVersionUID = 1L;
 	private static final String lat = "lat";
 	private static final String lon = "lon";
+	private static final String rad = "rad";
+	private static final String exp = "exp";
 	private static final String self = "self";
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		try {
-			Key userKey = getUserKey(OAuthServiceFactory.getOAuthService().getCurrentUser());
+			Key userKey = MosaicUser.getUserKey(OAuthServiceFactory.getOAuthService().getCurrentUser());
 			DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 			JSONObject responseObj = new JSONObject();
 			try {
-				responseObj.put(kind, jsonFromEntities(datastore.prepare(getMessagesQuery(req.getParameter(lat), req.getParameter(lon), req.getParameter(self), userKey)).asList(FetchOptions.Builder.withDefaults())));
+				responseObj.put(Message.kind, Message.jsonFromEntities(datastore.prepare(Message.getMessagesQuery(req.getParameter(lat), req.getParameter(lon), req.getParameter(self), userKey)).asList(FetchOptions.Builder.withDefaults())));
 				resp.setContentType("application/json");
 				resp.getWriter().write(responseObj.toString());
 			} catch (JSONException e) {
@@ -80,78 +63,17 @@ public class MessagesServlet extends HttpServlet {
 				e.printStackTrace();
 			}
 		} catch (OAuthRequestException e) {
-			e.printStackTrace();
 			resp.setStatus(401);
 		}
 	}
-	
-	private Query getMessagesQuery(String latitude, String longitude, String self, Key userKey) throws Exception {
-		Query query = new Query(kind);
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
 		try {
-			query.setFilter(getRangeFilter(latitude, longitude));
-			if (Boolean.parseBoolean(self))
-				query.setAncestor(userKey);
-		} catch (Exception e) {
-			if ((userKey != null) && Boolean.parseBoolean(self))
-				query.setAncestor(userKey);
-			else
-				throw new Exception("invalid parameters");
+			User user = OAuthServiceFactory.getOAuthService().getCurrentUser();
+		} catch (OAuthRequestException e) {
+			resp.setStatus(401);
 		}
-		return query;
-	}
-	
-	private Filter getRangeFilter(String latitude, String longitude) throws Exception {
-		if ((latitude != null) && (longitude != null))
-			return CompositeFilterOperator.and(getCoordinateFilter(latitude, latitude),
-					getCoordinateFilter(longitude, longitude));
-		else
-			throw new Exception("no coordinates");
-	}
-	
-	private Filter getCoordinateFilter(String coordinate, String type) {
-		Filter minFilter = new FilterPredicate(coordinate,
-				FilterOperator.GREATER_THAN_OR_EQUAL,
-				type + "-" + radius);
-		Filter maxFilter = new FilterPredicate(coordinate,
-				FilterOperator.LESS_THAN_OR_EQUAL,
-				type + "+" + radius);
-		return CompositeFilterOperator.and(minFilter, maxFilter);
-	}
-	
-	public static String jsonFromEntities(List<Entity> messages) throws Exception {
-		JSONArray jarr = new JSONArray();
-		for (Entity message : messages) {
-			try {
-				jarr.put(jsonFromEntity(message));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return jarr.toString();
-	}
-	
-	public static String jsonFromEntity(Entity message) throws Exception {
-		JSONObject jobj = new JSONObject();
-		jobj.put(key, message.getKey());
-		jobj.put(body, message.getProperty(body));
-		jobj.put(created, message.getProperty(created));
-		jobj.put(latitude, message.getProperty(latitude));
-		jobj.put(longitude, message.getProperty(longitude));
-		jobj.put(radius, message.getProperty(radius));
-		jobj.put(expiry, message.getProperty(expiry));
-		jobj.put(user, message.getProperty(user));
-		return jobj.toString();
-	}
-	
-	private Key getUserKey(User user) {
-		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-		Filter filter = new FilterPredicate(userid, FilterOperator.EQUAL, user.getUserId());
-		Query query = new Query("user").setFilter(filter);
-		List<Entity> users = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(1));
-		if (users.isEmpty())
-			return null;
-		else
-			return users.get(0).getKey();
 	}
 
 }
